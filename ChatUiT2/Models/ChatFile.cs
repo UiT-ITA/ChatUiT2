85108d1f14e9a5084a7ee3155bc283338dcc2818
Øystein Tveito <oystein.tveito@uit.no>
2024-06-24 16:39:48 +0200

    More progress in fileupload

---
+++ ChatUiT2/Models/ChatFile.cs
@@ -0,0 +1,42 @@
+﻿namespace ChatUiT2.Models;
+
+public class ChatFile
+{
+    public string FileName { get; set; }
+    public FileType FileType { get; set; }
+    public byte[]? Bytes { get; set; }
+
+    public static FileType GetFileTypeFromName(string name)
+    {
+        string extention = name.Split('.').Last();
+        if (ImageFiles.Contains(extention))
+        {
+            return FileType.Image;
+        }
+        else if (DataFiles.Contains(extention))
+        {
+            return FileType.Data;
+        }
+        else if (DocumentFiles.Contains(extention))
+        {
+            return FileType.Document;
+        }
+        else
+        {
+            return FileType.Document;
+        }
+    }
+
+    public static List<string> ImageFiles = new() { "jpg", "png", "bmp" };
+    public static List<string> DataFiles = new() { "xlsx", "csv", "json" };
+    public static List<string> DocumentFiles = new() { "pdf", "txt", "docx"};
+    public static List<string> AllFiles = ImageFiles.Concat(DataFiles).Concat(DocumentFiles).ToList();
+}
+
+public enum FileType
+{
+    Image,
+    Data,
+    Document
+}
+
--- ChatUiT2/Services/StorageService.cs
+++ ChatUiT2/Services/StorageService.cs
@@ -1,5 +1,7 @@
-﻿using Azure.Storage.Blobs;
+﻿using Amazon.SecurityToken.Model;
+using Azure.Storage.Blobs;
 using ChatUiT2.Interfaces;
+using ChatUiT2.Models;
 
 namespace ChatUiT2.Services;
 
@@ -17,7 +19,7 @@ public class StorageService
         _blobServiceClient = new BlobServiceClient(connectionString);
     }
 
-    public async Task<byte[]?> GetFileBytes(string username, string filename)
+    private async Task<byte[]?> GetFileBytes(string username, string filename)
     {
         try
         {
@@ -35,12 +37,64 @@ public class StorageService
         }
     }
 
-    public async Task UploadFile(string username, string filename, byte[] bytes)
+    private async Task UploadFile(string username, string filename, byte[] bytes)
     {
         var containerClient = _blobServiceClient.GetBlobContainerClient(username);
         await containerClient.CreateIfNotExistsAsync();
         var blobClient = containerClient.GetBlobClient(filename);
         await blobClient.UploadAsync(new MemoryStream(bytes));
+    }
+
+    public async Task DeleteFile(string username, string filename)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(username);
+        var blobClient = containerClient.GetBlobClient(filename);
+        await blobClient.DeleteIfExistsAsync();
+    }
+
+    public async Task<IEnumerable<string>> ListFiles(string username)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(username);
+        var files = new List<string>();
+        await foreach (var blobItem in containerClient.GetBlobsAsync())
+        {
+            files.Add(blobItem.Name);
+        }
+        return files;
+    }
+
+    public async Task DeleteContainer(string username)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(username);
+        await containerClient.DeleteIfExistsAsync();
+    }
+
+    
+
+    public async Task<ChatFile> GetFile(string username, string filename)
+    {
+        ChatFile file = new()
+        {
+            FileName = filename,
+            FileType = ChatFile.GetFileTypeFromName(filename),
+            Bytes = await GetFileBytes(username, filename)
+        };
+
+        if (file.Bytes == null)
+        {
+            throw new Exception("File not found");
+        }
+
+        return file;
+    }
+
+    public async Task UploadFile(string username, ChatFile file)
+    {
+        if (file.Bytes == null)
+        {
+            throw new Exception("File bytes are null");
+        }
+        await UploadFile(username, file.FileName, file.Bytes);
     }
 
 
