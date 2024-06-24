edb02a88ba9a2f2045d2aa64554885a332e1da66
Øystein Tveito <oystein.tveito@uit.no>
2024-06-24 22:48:15 +0200

    Fix support for linux in 2d graphics

--- ChatUiT2/Models/ChatFile.cs
+++ ChatUiT2/Models/ChatFile.cs
@@ -2,7 +2,7 @@
 
 public class ChatFile
 {
-    public string FileName { get; set; }
+    public string FileName { get; set; } = "";
     public FileType FileType { get; set; }
     public byte[]? Bytes { get; set; }
 
