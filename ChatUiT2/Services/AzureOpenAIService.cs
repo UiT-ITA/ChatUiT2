a7fa64955c9f6a5764ce8b2659de9da4958f2265
Øystein Tveito <oystein.tveito@uit.no>
2024-06-27 11:19:57 +0200

    images and txt files can now be sent to azure openai

--- ChatUiT2/Services/AzureOpenAIService.cs
+++ ChatUiT2/Services/AzureOpenAIService.cs
@@ -61,7 +61,8 @@ public static class AzureOpenAIService
                 continue;
             }
 
-            OpenAI.Chat.ChatMessage requestMessage = GetOpenAIMessage(message);
+            //OpenAI.Chat.ChatMessage requestMessage = GetOpenAIMessage(message);
+            OpenAI.Chat.ChatMessage requestMessage = ChatMessageWithFiles(message);
 
 
             messages.Insert(1, requestMessage);
@@ -90,6 +91,54 @@ public static class AzureOpenAIService
         {
             throw new Exception("Unkown message role");
         }
+    }
+
+    public static OpenAI.Chat.ChatMessage ChatMessageWithFiles(Models.ChatMessage message)
+    {
+        if (message.Role != Models.ChatMessageRole.User) {
+            return new AssistantChatMessage(message.Content);
+        }
+
+        List<ChatMessageContentPart> messageContentParts = new List<ChatMessageContentPart>();
+        foreach (var file in message.Files)
+        {
+            Console.WriteLine("Adding file: " + file.FileName);
+            if (file.Bytes == null)
+            {
+                throw new Exception("File is empty");
+            }
+
+            if (file.FileType == FileType.Image)
+            {
+                var messagePart = ChatMessageContentPart.CreateImageMessageContentPart(
+                    imageBytes: new BinaryData(file.Bytes!),
+                    imageBytesMediaType: "image/" + file.FileName.Split(".").Last()
+                    );
+                messageContentParts.Add(messagePart);
+            }
+            else if (file.FileType == FileType.Document)
+            {
+                if (file.FileName.Split(".").Last() == "txt")
+                {
+                    string text = System.Text.Encoding.UTF8.GetString(file.Bytes!);
+                    var messageText = "This is a file named " + file.FileName + ":\n" + text + "\n\n";
+
+                    var messagePart = ChatMessageContentPart.CreateTextMessageContentPart(messageText);
+                    messageContentParts.Add(messagePart);
+                }
+                else
+                {
+                    throw new Exception("Unsupported document type: " + file.FileName.Split(".").Last());
+                }
+            }
+            else if (file.FileType == FileType.Data)
+            {
+            }
+        }
+
+        messageContentParts.Add(ChatMessageContentPart.CreateTextMessageContentPart(message.Content));
+
+        return new UserChatMessage(messageContentParts);
     }
 
     public static int GetTokens(string model, string content)
