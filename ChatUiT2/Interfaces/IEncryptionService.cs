5d156b63079f98dfb29939d9c802561394b506ed
Øystein Tveito <oystein.tveito@uit.no>
2024-11-15 12:27:36 +0100

    Changed encryption function definitions

--- ChatUiT2/Interfaces/IEncryptionService.cs
+++ ChatUiT2/Interfaces/IEncryptionService.cs
@@ -3,8 +3,10 @@ namespace ChatUiT2.Interfaces;
 
 public interface IEncryptionService
 {
+    byte[] Encrypt(byte[] data, byte[] key);
     byte[] Encrypt(string data, byte[] key);
-    string Decrypt(byte[] encryptedData, byte[] key);
+    byte[] Decrypt(byte[] encryptedData, byte[] key);
+    string DecryptString(byte[] encryptedData, byte[] key);
     byte[] GetEncryptionKeyForAes256(byte[] password, byte[] salt, int iterations);
     byte[] GetRandomByteArray(int length);
 }
