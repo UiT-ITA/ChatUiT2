b2db4c0ab0b7af7451a1ba597592f93599841325
tveito <oystein@tveito.cc>
2024-11-21 09:44:17 +0100

    Created instruction model

---
+++ ChatUiT2/Models/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using ChatUiT2.Tools;
+
+namespace ChatUiT2.Models;
+
+public class Instruction
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public bool IsFavorite { get; set; } = false;
+    public InstructionContent Content { get; set; } = new InstructionContent();
+}
+
+public class InstructionContent
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Owner { get; set; } = "";
+    public string Name { get; set; } = "New Instruction";
+    public string Description { get; set; } = "";
+    public string Content { get; set; } = "";
+    public DateTime Created { get; set; } = DateTimeTools.GetTimestamp();
+    public List<string> SharedWith { get; set; } = new List<string>();
+}
