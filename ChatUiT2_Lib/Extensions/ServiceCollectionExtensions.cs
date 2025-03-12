922674c48e9369ceb538c10ccc626fafdccd9d23
Espen Rivedal <espen.rivedal@uit.no>
2025-03-12 19:03:18 +0100

    Added extension method for keyed singletons

---
+++ ChatUiT2_Lib/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatUiT2.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    private static readonly Dictionary<string, object> _namedSingletons = new Dictionary<string, object>();
+
+    public static IServiceCollection AddKeyedSingleton<TService>(this IServiceCollection services, string key, Func<IServiceProvider, TService> implementationFactory) where TService : class
+    {
+        services.AddSingleton(provider =>
+        {
+            if (!_namedSingletons.ContainsKey(key))
+            {
+                _namedSingletons[key] = implementationFactory(provider);
+            }
+            return (TService)_namedSingletons[key];
+        });
+        return services;
+    }
+
+    public static TService GetKeyedSingleton<TService>(this IServiceProvider provider, string key) where TService : class
+    {
+        return (TService)_namedSingletons[key];
+    }
+}
